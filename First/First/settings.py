4b9b2edbf009e6c0ed28785ed2b7b38b2e09b785
Udayraj Ghorpade <udayrajghorpade2503@gmail.com>
2024-10-01 22:20:53 +0530

    True debug

--- First/First/settings.py
+++ First/First/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ivjstf%iux+or1!ls)6!u=l%wfvhwcno9(lellzlzj3!=!ua8+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS=['*']
 CORS_ORIGIN_ALLOW_ALL = True
